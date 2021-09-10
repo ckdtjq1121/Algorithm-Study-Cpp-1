80db2f84dc0f88d7e2fce16de18eeb377302472d
canonbean1 <80ckdtjq@naver.com>
2021-09-10 16:59:33 +0900

    - implement reverse insert sorted - O(n^2) - segTree - O(n log n) - unionfind - O(n^2) (not yet)

--- algospot/INSERTION/INSERTION/소스.cpp
+++ algospot/INSERTION/INSERTION/소스.cpp
@@ -2,9 +2,82 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-const int MAX = 50000;
+const int MAX = 50001;
+int n;
+
+int ans[MAX];
 int arr[MAX];
 int sorted[MAX];
+int segTree[MAX * 2 + 1];
+bool checked[MAX];
+void makeReverse()
+{
+	int tmp;
+	for (int i = n - 1; i >= 0; i--)
+	{
+		for (int j = i - arr[i]; j < i; j++)
+		{
+			tmp = sorted[j];
+			sorted[j] = sorted[j + 1];
+			sorted[j + 1] = tmp;
+		}
+
+	}
+}
+
+void treeInit(int node, int start, int end)
+{
+	if (start == end)
+	{
+		segTree[node] = 1;
+		return;
+	}
+
+	int mid = (start + end) / 2;
+	treeInit(2 * node, start, mid);
+	treeInit(2 * node + 1, mid + 1, end);
+	segTree[node] = segTree[2 * node] + segTree[2 * node + 1];
+
+	return;
+}
+// [start, end]에서 x번째 원소의 index 찾기
+int query(int node, int start, int end, int x)
+{
+	if (start == end) return start; // x번째 원소 index return
+	int mid = (start + end) / 2;
+	if (segTree[2 * node] < x) query(2 * node + 1, mid + 1, end, x - segTree[2 * node]); // 왼쪽 노드의 값 < x 오른쪽 노드에서 (x - 왼쪽 노드의 값) 번째 값을 찾아야함
+	else return query(2 * node, start, mid, x);	// 왼쪽노드에서  x번째 값을 찾으면 됨
+}
+void update(int node, int start, int end, int x)
+{
+	if (start > x || end < x) return; // x가 없는 범위면 끝을 냄
+	segTree[node]--;	// x가 있는 범위에엇 x를 사용했으므로 cnt를 -- 해줌
+	if (start == end) return;	// 마지막 update 범위
+	int mid = (start + end) / 2;
+	update(2 * node, start, mid, x); // 왼쪽 자식 트리
+	update(2 * node + 1, mid + 1, end, x); // 오른쪽 자식 트리
+}
+
+// sorted[x] == x;
+int find(int x)	// 뒤에서 x번째 원소 찾기
+{
+	int& ret = sorted[n - x];	// x번째 값
+	int ans;
+	if (x == n)
+		return 1;
+
+	ans = ret;
+	ret = sorted[n - x - 1];	// make union
+	if (checked[ans]) // not union
+	{
+		ans = find(n - ret);		// find root of union
+		
+	}
+
+	checked[ans] = true;
+	return ans;
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -14,37 +87,46 @@ int main()
 
 	for (; testCase > 0; testCase--)
 	{
-		int n;
+		//input 
 		cin >> n;
-		for (int i = 0; i < n; i++)
+		for (int i = 1; i <= n; i++)
 		{
 			cin >> arr[i];
 		}
 
-		for (int i = 0; i < n; i++)
+		//init
+		for (int i = 1; i <= n; i++)
 		{
-			sorted[i] = i + 1;
+			sorted[i] = i;
 		}
 
-		int tmp;
-		for (int i = n - 1; i >= 0; i--)
-		{
-			for (int j = i - arr[i]; j < i; j++)
-			{
-				tmp = sorted[j];
-				sorted[j] = sorted[j + 1];
-				sorted[j + 1] = tmp;
-			}
+		// inplement ReverseINSERTION  -  n^2
+		//makeReverse();
 
+		// segTree	-  n * log n
+		//treeInit(1, 1, n);
+		//for (int i = n; i > 0; i--) {
+		//	ans[i] = query(1, 1, n, arr[i] + 1);
+		//	update(1, 1, n, ans[i]);
+		//	ans[i] = n - ans[i] + 1;
+		//}
+
+		// unionfind		-  fail 
+		for (int i = 1; i <= n; i++)
+		{
+			checked[i] = false;
+		}
+		for (int i = n; i > 0; i--)
+		{
+			ans[i] = find(arr[i]);
 		}
 
-		for (int i = 0; i < n; i++)
+		//output
+		for (int i = 1; i <= n; i++)
 		{
-			cout << sorted[i] << " ";
+			cout << ans[i] << " ";
 		}
 		cout << endl;
 	}
 }
 
-// insert sorting  : i - 1 까지 정렬되있을 대 i번째 원소를 i - 1까지 중 원소 사이에 삽입을 하며 정렬을함
-// reverse  -> 정렬된 i번째 원소를 i의 원래 위치로 되돌려냄

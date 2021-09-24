7a7642914f14e088fbb57c9a1a3320809eab5667
canonbean1 <80ckdtjq@naver.com>
2021-09-24 10:56:08 +0900

    입력 개수가 정해지지 않았을 때 - cin >> a 의 값을 조건문으로 사용하기 - cin.eof() 사용하기

---
+++ else/RandomInputSize/RandomInputSize/소스.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int a, b;
+
+    while (cin >> a >> b)
+        cout << a + b << "\n";
+
+    //while (true)
+    //{
+    //    cin >> a >> b;
+    //    if (!cin.eof())
+    //        break;
+    //    cout << a + b << "\n";
+    //}
+
+    return 0;
+}
